a3cf0670a890e122b5a6c260d4632ee7b3b30ac8
juantorres9 <juan.torres.zetino@gmail.com>
2020-05-24 19:09:04 +0200

    seconde commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 echo "initialisation de script"
 PARAM1=$1
 PARAM2=$2
+PARAM3=$3
 printf "parametre reçu 1 =$PARAM1 \n"
 printf "parametre reçu 2 =$PARAM2 \n"
+printf "parametre reçu 3 =$PARAM3 \n"
+
