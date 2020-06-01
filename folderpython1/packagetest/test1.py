b3d6d3596450c85256fe4be3f2179a1100f67218
jctorresrisk <jctorresrisk@jctorresrisk-VirtualBox>
2020-06-01 16:45:22 +0200

    python script

---
+++ folderpython1/packagetest/test1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+import sys
+
+from _curses import version
+
+username=input("Veuillez ecrire votre nom")
+
+print("bonjour ",username)
+
+print ("configuration info ",sys.version)
