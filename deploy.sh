38f0540333434b5dd96651c2d8eb856036366cb2
jctorreszetino <ext-juan-carlos.torres-zetino@flowbird.group>
2020-07-18 15:44:19 +0200

    adding test information

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ printf "parametre reçu 1 =$PARAM1 \n"
 printf "parametre reçu 2 =$PARAM2 \n"
 printf "parametre reçu 3 =$PARAM3 \n"
 
-printf "execution script python"
+printf "execution script python:\n ./folderpython1/packagetest/test1.py -a $PARAM1 -b $PARAM2 \n"
 python3 ./folderpython1/packagetest/test1.py -a $PARAM1 -b $PARAM2 
 printf "exit fin script \n"
 
